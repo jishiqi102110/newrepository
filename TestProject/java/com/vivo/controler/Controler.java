405277452593099fea1ea2af17dea26d9ca1da37
liangweitao <liangweitao@bbktel.com>
2017-11-22 20:55:25 +0800

    rete

--- TestProject/java/com/vivo/controler/Controler.java
+++ TestProject/java/com/vivo/controler/Controler.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class Controler {
+	@Autowired
+	private KafkaTemplate template;
 	@RequestMapping("test")
 	public void test(){
 		System.out.println("success");;
